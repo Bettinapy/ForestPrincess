d7161cabf7e4ff861664f6c00cf10781f5d4eb70
Yuanyuan Zhou <bettinazhounyu@gmail.com>
2020-08-23 19:24:56 -0400

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import "./styles/index.scss";
 import './scripts/loaders';
 import Game from './scripts/game';
-import { loadLevel } from "./scripts/loaders";
-import { 
-  createGroundLayer,
-  createMainBgLayer, 
-  createCharacterLayer,
-  createDashboardLayer,
-  createTileMatrix
-} from './scripts/levels/level-draw';
-import {
-  loadBackgroundLayers,
-  loadBackgroundTiles,
-} from "./scripts/sprite-load";
-import Layer from './scripts/levels/layers';
-import {createPrincessIdle} from './scripts/princess';
-import {createEnemyWalk} from './scripts/enemies';
-import FixedTimeLoop from './scripts/fixed-time-loop';
-import KeyboardInput from './scripts/keyboard-input';
-import Camera from './scripts/camera';
-import Sound from './scripts/dashboard/sound';
 
 document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementsByTagName("canvas")[0];
@@ -38,55 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   })
 
-  // Promise.all([
-  //   loadBackgroundLayers(),
-  //   loadBackgroundTiles(),
-  //   loadLevel("level-1"),
-  //   createPrincessIdle(),
-  //   createEnemyWalk(),
-  // ]).then(([mainBg, jungleTiles, level, princessIdle, enemy]) => {
-    
-  //   enemy.pos.setVector(600,0)
 
-  //   // push to layers array and draw
-  //   const layer = new Layer();
-  //   layer.layers.push(createMainBgLayer(mainBg));
-  //   layer.layers.push(createGroundLayer(level.backgrounds, jungleTiles));
-  //   layer.layers.push(createCharacterLayer(princessIdle));
-  //   layer.layers.push(createCharacterLayer(enemy));
-  //   layer.layers.push(createDashboardLayer(princessIdle))
-    
-  //   // add to characters
-  //   layer.characters.add(princessIdle);
-  //   layer.characters.add(enemy);
-
-  //   // create matrix
-  //   createTileMatrix(level.backgrounds, layer.tiles)
-   
-  //   // scrolling camera
-  //   const camera = new Camera;
-
-  //   // keyboard input
-  //   const input = new KeyboardInput(princessIdle);
-  //   input.listenKeys(window);
-
-  //   // background music
-  //   const bgMusic = new Sound("src/audios/background.ogg")
-  //   //bgMusic.loop();
-  //   bgMusic.play();
-
-  //   const fixedLoop = new FixedTimeLoop();
-  //   fixedLoop.update = function update(timestep){
-
-  //     layer.update(timestep, camera);
-
-  //     if (princessIdle.pos.x > 300) {
-  //       camera.pos.x = princessIdle.pos.x - 300;
-  //     }
-  //     layer.draw(ctx, camera);
-      
-  //   }
-  //   fixedLoop.start();
-  // });
 
 });
